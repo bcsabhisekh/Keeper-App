eeb9cb0adf2e68e4ab9d73abf136b1daa8a736d3
Abhisekh <y.abhisekhmessi535@gmail.com>
2022-06-08 04:23:03 +0530

    Second Commit

---
+++ build/precache-manifest.ebaad066d9311406fe03ad56b3986d30.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0cefe33b3ee9011816f1db914d58c1",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "e2bbe1d731869b079277",
+    "url": "/Keeper-App/static/js/2.265bff3e.chunk.js"
+  },
+  {
+    "revision": "b86b80a7ff44714e0771",
+    "url": "/Keeper-App/static/js/main.b6d33ab2.chunk.js"
+  },
+  {
+    "revision": "8c85bae05d866358124a",
+    "url": "/Keeper-App/static/js/runtime-main.fca5ae5c.js"
+  }
+]);
